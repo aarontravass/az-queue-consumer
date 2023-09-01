335de70b44a7f61202622291e13870a48836b360
aarontravass <aaron.travass@hotmail.com>
2023-09-01 18:58:42 -0400

    fix: fixed tests

--- tests/consumer.test.ts
+++ tests/consumer.test.ts
@@ -9,10 +9,11 @@ vi.mock('@azure/storage-queue', () => {
   const QueueServiceClient = vi.fn()
   QueueServiceClient['fromConnectionString'] = vi.fn((...args) => {
     const getQueueClient = vi.fn(() => {
-      const createIfNotExists = vi.fn(() => new Promise((resolve) => resolve('this')))
-      const receiveMessages = vi.fn(() => new Promise((resolve) => resolve(queueService.fetchMessages(2))))
+      const createIfNotExists = vi.fn(() => new Promise((resolve) => resolve(queueService.createQueue())))
+      const receiveMessages = vi.fn(() => new Promise((resolve) => resolve(queueService.fetchNMessages(2))))
       const deleteMessage = vi.fn(
-        (messageId: string, popReceipt: string) => new Promise((resolve) => resolve(messageId))
+        (messageId: string, popReceipt: string) =>
+          new Promise((resolve) => resolve(queueService.deleteMessage(messageId)))
       )
       return { createIfNotExists, receiveMessages, deleteMessage }
     })
@@ -25,6 +26,7 @@ vi.mock('@azure/storage-queue', () => {
 describe('azure queue tests', () => {
   afterEach(() => {
     vi.clearAllMocks()
+    queueService.restoreMessages()
   })
   afterAll(() => {
     vi.resetAllMocks()
@@ -37,7 +39,10 @@ describe('azure queue tests', () => {
       listener.on('queue::ready', handler)
       await flushPromises()
       expect(handler).toBeCalledTimes(1)
-      expect(handler).toHaveBeenCalledWith('this')
+      expect(handler).toHaveBeenCalledWith({
+        _response: undefined,
+        succeeded: true
+      })
     })
     it.skip('should throw an error if queue could not be initialized', async () => {
       expect.assertions(1)
@@ -45,7 +50,7 @@ describe('azure queue tests', () => {
       const errorToThrow = { code: 'code', message: 'message' }
 
       try {
-        vi.spyOn(QueueServiceClient, 'fromConnectionString').mockImplementation((...args: string[]) => {
+        vi.spyOn(QueueServiceClient, 'fromConnectionString').mockImplementation((..._args: string[]) => {
           const getQueueClient = vi.fn(() => {
             const createIfNotExists = vi.fn(() => new Promise((resolve, reject) => reject(errorToThrow)))
 
@@ -64,24 +69,24 @@ describe('azure queue tests', () => {
   describe('listener tests', () => {
     it('should test for queue message', async () => {
       expect.assertions(2)
-      const handler = vi.fn((messages) => {})
+      const handler = vi.fn((_messages) => {})
       const listener = new AzureQueueConsumer('test', 'https://test.com', handler)
       listener.listen()
       await flushPromises()
       expect(handler).toHaveBeenCalledTimes(1)
-      expect(handler).toHaveBeenCalledWith(queueService.fetchMessages(2).receivedMessageItems)
+      expect(handler).toHaveBeenCalledWith(queueService.fetchNMessages(2).receivedMessageItems)
     })
     describe('listener event tests', () => {
       it('should emit event when message is received', async () => {
         expect.assertions(2)
-        const handler = vi.fn((messages) => {})
+        const handler = vi.fn((_messages) => {})
         const listener = new AzureQueueConsumer('test', 'https://test.com', (...args) => {})
         listener.on('message::onReceive', handler)
         listener.listen()
 
         await flushPromises()
         expect(handler).toBeCalledTimes(1)
-        expect(handler).toHaveBeenCalledWith(queueService.fetchMessages(2))
+        expect(handler).toHaveBeenCalledWith(queueService.fetchNMessages(2))
       })
       it('should emit event when handler completes execution', async () => {
         expect.assertions(2)
@@ -95,7 +100,7 @@ describe('azure queue tests', () => {
       })
       it('should emit event when handler fails', async () => {
         expect.assertions(2)
-        const handler = vi.fn((messages) => {})
+        const handler = vi.fn((_messages) => {})
         const listener = new AzureQueueConsumer('test', 'https://test.com', (...args) => {
           throw 'error'
         })
@@ -112,7 +117,7 @@ describe('azure queue tests', () => {
     describe('delete message fn event tests', () => {
       it('should emit an event before deleting a specific message', async () => {
         expect.assertions(3)
-        const handler = vi.fn((messages) => {})
+        const handler = vi.fn((_messages) => {})
         const listener = new AzureQueueConsumer('test', 'https://test.com', (...args) => {})
         listener.on('message::preDelete', handler)
         listener.listen()
@@ -121,31 +126,31 @@ describe('azure queue tests', () => {
 
         expect(handler).toHaveBeenNthCalledWith(
           1,
-          queueService.fetchMessages(2).receivedMessageItems[0].messageId,
-          queueService.fetchMessages(2).receivedMessageItems[0].popReceipt
+          queueService.fetchNMessages(2).receivedMessageItems[0].messageId,
+          queueService.fetchNMessages(2).receivedMessageItems[0].popReceipt
         )
         expect(handler).toHaveBeenNthCalledWith(
           2,
-          queueService.fetchMessages(2).receivedMessageItems[1].messageId,
-          queueService.fetchMessages(2).receivedMessageItems[1].popReceipt
+          queueService.fetchNMessages(2).receivedMessageItems[1].messageId,
+          queueService.fetchNMessages(2).receivedMessageItems[1].popReceipt
         )
         expect(handler).toHaveBeenCalledTimes(2)
       })
       it('should emit an event after deleting a specific message', async () => {
         expect.assertions(3)
-        const handler = vi.fn((messages) => {})
+        const handler = vi.fn((_messages) => {})
         const listener = new AzureQueueConsumer('test', 'https://test.com', (...args) => {})
         listener.on('message::afterDelete', handler)
         listener.listen()
         await flushPromises()
-        expect(handler).toHaveBeenNthCalledWith(
-          1,
-          queueService.fetchMessages(2).receivedMessageItems[0].messageId
-        )
-        expect(handler).toHaveBeenNthCalledWith(
-          2,
-          queueService.fetchMessages(2).receivedMessageItems[1].messageId
-        )
+        expect(handler).toHaveBeenNthCalledWith(1, {
+          _response: undefined,
+          errorCode: undefined
+        })
+        expect(handler).toHaveBeenNthCalledWith(2, {
+          _response: undefined,
+          errorCode: undefined
+        })
         expect(handler).toHaveBeenCalledTimes(2)
       })
     })
