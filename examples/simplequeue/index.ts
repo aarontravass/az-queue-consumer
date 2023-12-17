3ef6cf4637d0c0f06cdfec5bf0de692b89c9ca19
aarontravass <aaron.travass@hotmail.com>
2023-12-17 17:59:58 -0500

    refactor: added node EOL in example

--- examples/simplequeue/index.ts
+++ examples/simplequeue/index.ts
@@ -1,24 +1,24 @@
 import { AzureQueueConsumer } from 'az-queue-consumer'
 import { readFileSync } from 'node:fs'
+import { EOL } from 'node:os'
 
 // read .env file
-;(() => {
-  const file = readFileSync('./.env', { encoding: 'utf-8' }).toString()
-  file.split('\r\n').forEach((env) => {
-    for (let end = 0; end < env.length; end++) {
-      if (env.charAt(end) == '=') {
-        const key = env.slice(0, end)
-        let value = env.slice(end + 1)
-        if (value.charAt(0) == "'") value = value.slice(1)
-        if (value.charAt(value.length - 1) == "'") value = value.slice(0, value.length - 1)
-        process.env[key] = value
-        break
+;(() =>
+  readFileSync('./.env', { encoding: 'utf-8' })
+    .toString()
+    .split(EOL)
+    .forEach((env) => {
+      for (let end = 0; end < env.length; end++) {
+        if (env.charAt(end) == '=') {
+          const key = env.slice(0, end)
+          let value = env.slice(end + 1)
+          if (value.charAt(0) == "'") value = value.slice(1)
+          if (value.charAt(value.length - 1) == "'") value = value.slice(0, value.length - 1)
+          process.env[key] = value
+          break
+        }
       }
-    }
-  })
-})()
-
-
+    }))()
 
 const client = new AzureQueueConsumer(
   process.env.AZURE_QUEUE_NAME!,
